a56838acb1635b8b5c48581e317330fb4d3f8dc1
Sunny <sundeepkmanik@gmail.com>
2018-06-07 14:35:45 -0700

    controller done i guess

--- config/orm.js
+++ config/orm.js
@@ -4,24 +4,30 @@ var connection = require("../config/connection.js");
 // helper function bc example activity had it idk
 function printQuestionMarks(num) {
     var arr = [];
+
     for (var i = 0; i < num; i++) {
         arr.push("?");
     }
+
     return arr.toString();
 }
 
 // helper function bc example activity had it idk
 function objToSql(ob) {
     var arr = [];
+
     for (var key in ob) {
         var value = ob[key];
+
         if (Object.hasOwnProperty.call(ob, key)) {
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
             }
+
             arr.push(key + "=" + value);
         }
     }
+
     return arr.toString();
 }
 
@@ -29,10 +35,14 @@ var orm = {
     // selectAll()
     selectAll: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
+
+        console.log(queryString);
+
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
             }
+
             cb(result);
         });
     },
@@ -65,13 +75,14 @@ var orm = {
         queryString += objToSql(objColVals);
         queryString += " WHERE ";
         queryString += condition;
-    
+
         console.log(queryString);
+
         connection.query(queryString, function(err, result) {
           if (err) {
             throw err;
           }
-    
+          
           cb(result);
         });
     }
